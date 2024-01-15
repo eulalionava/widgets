79f0b043935f714d8c40606b3599f874e8dfec8b
Eulalio <eulalio@panoram.finance>
2024-01-14 22:47:38 -0600

    progress

--- lib/presentation/screens/progress/progress_screen.dart
+++ lib/presentation/screens/progress/progress_screen.dart
@@ -44,18 +44,28 @@ class _ControlledProgresIndicator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CircularProgressIndicator(value: 0.5,strokeWidth: 2,backgroundColor: Colors.black12),
-          const SizedBox(width: 20),
-          Expanded(
-            child: LinearProgressIndicator(value: 0.5),
-          )
-        ],
-      ),
+    return  StreamBuilder(
+      stream: Stream.periodic(const Duration(milliseconds: 300),(value){
+        return (value * 2) / 100;
+      }).takeWhile((value) => value < 100),
+      builder: (context, snapshot) {
+
+        final progressValue = snapshot.data ?? 0;
+
+        return Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircularProgressIndicator(value: progressValue,strokeWidth: 2,backgroundColor: Colors.black12),
+              const SizedBox(width: 20),
+              Expanded(
+                child: LinearProgressIndicator(value: progressValue),
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
